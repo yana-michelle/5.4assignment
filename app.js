422d0d8a43e5a921b5e9d0b66c0efb135f9fceeb
yana-michelle <60404658+yana-michelle@users.noreply.github.com>
2022-02-18 21:48:30 -0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ const names= ['Ed', 'John', 'Maria', 'Eliza', 'Burrito', 'Harry', 'Potter'];
 
 
 for (name of names) {
+    console.log(name)
     if(name === 'Maria'){
-        console.log("Maria is in my list")
+        console.log("Maria is in my list");
+        break;
     }
+}
+
+
+
+let loading = 0;
+
+while(loading < 100){
+    console.log('Websitr is still loading');
+
+    loading ++;
 }
