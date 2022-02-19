73e50c998e3556b8de7ee4e13ad6e0fa213662a4
yana-michelle <60404658+yana-michelle@users.noreply.github.com>
2022-02-18 21:42:04 -0600

    trying for loops

---
+++ app.js
@@ -0,0 +1,8 @@
+const names= ['Ed', 'John', 'Maria', 'Eliza', 'Burrito', 'Harry', 'Potter'];
+
+
+for (name of names) {
+    if(name === 'Maria'){
+        console.log("Maria is in my list")
+    }
+}
